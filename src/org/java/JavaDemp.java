9be7d702c98bc18434564b26caf6d1d8cb58d5a7
Monisha Sachidhanandham <moniisha0220@gmail.com>
2022-11-21 20:06:39 +0530

    updated document

--- src/org/java/JavaDemp.java
+++ src/org/java/JavaDemp.java
@@ -3,5 +3,9 @@ package org.java;
 public class JavaDemp {
 public static void main(String[] args) {
 	System.out.println("Hello Git");
+	int a = 3;
+	int b = 9 ;
+	int c = a + b ;
+	System.out.println(c);
 }
 }
