90420384225670cba184ac1e1393e92da176d156
Monisha Sachidhanandham <80873484+Moniisha20@users.noreply.github.com>
2022-11-21 20:08:49 +0530

    Update JavaDemp.java

--- src/org/java/JavaDemp.java
+++ src/org/java/JavaDemp.java
@@ -5,7 +5,10 @@ public static void main(String[] args) {
 	System.out.println("Hello Git");
 	int a = 3;
 	int b = 9 ;
+	int d = 7 ;
+	int e = 13 ;
 	int c = a + b ;
+	int f = d * e ;
 	System.out.println(c);
 }
 }
